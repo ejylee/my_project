9417ee34e2aada990aaa216e00d0129ca649f5b2
jungyoo <dallim0524@gmail.com>
2021-01-06 00:20:18 +0900

    v.0.2
    크롤링한 값들을 리스트 형태로 변형함

--- prac.py
+++ prac.py
@@ -6,64 +6,38 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-# 타겟 URL을 읽어서 HTML 받아오
+# ?! 기존 db에 저장되어있던 최신값 가져오기 어떻게하지? -> db.foo.find().sort({x:-1})
+# ?! 가져온 값들을 리스트형태로 어떻게 만들지? -> dbBags 리스트 만들기
+
+# 새로 크롤링하기 (newBags 리스트 만들기)
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.freitag.ch/ko/f41?items=showall',headers=headers)
-
-# 브라우저 제어하여 크기롤링 해오기
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-
 path = "./chromedriver"
 driver = webdriver.Chrome(path)
 driver.get("https://www.freitag.ch/ko/f41?items=showall")
 time.sleep(3)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+bags = soup.select('#products-selector > ul > li')
+productId = []
+dataDimension17 = []
+dataDimension18 = []
+img = []
+for bag in bags:
+    productId.append(bag['data-product-id'])
+    dataDimension17.append(bag['data-dimension17'])
+    dataDimension18.append(bag['data-dimension18'])
+    img.append(bag.select_one('img'))
+newBags = {'productId': productId}
+print(newBags)
 
-# 크롤링 해온 정보 튜닝해서 DB에 집어넣기
-#soup = BeautifulSoup(driver.page_source, 'html.parser')
-#bags = soup.select('#products-selector > ul > li')
-#for bag in bags:
-#    productId = bag['data-product-id']
-#    dataDimension17 = bag['data-dimension17']
-#    dataDimension18 = bag['data-dimension18']
-#    img = bag.select_one('img')
-    #print(productId,dataDimension17,dataDimension18,img) 크롤링 결과값 확인해보기
-#    doc = {
-#       'productId': productId,
-#        'dataDimension17': dataDimension17,
-#        'dataDimension18': dataDimension18,
-#        'img': img["src"]
-#    }
-#    db.bags.insert_one(doc)
-#driver.quit()
-
-#DB에 넣어둔 값과 새로 크롤링한 값 비교하기
-while True :
-    dbBags = list(db.bags.find({}))
-    #print(dbBags) db에 저장된 값이 잘 불러와지는지 확인
-    time.sleep(3)
-    newBags = []
-    path = "./chromedriver"
-    driver = webdriver.Chrome(path)
-    driver.get("https://www.freitag.ch/ko/f41?items=showall")
-    time.sleep(3)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    bags = soup.select('#products-selector > ul > li')
-    for bag in bags:
-        productId = bag['data-product-id']
-    dataDimension17 = bag['data-dimension17']
-    dataDimension18 = bag['data-dimension18']
-    img = bag.select_one('img')
-    doc = {
-        'productId': productId,
-        'dataDimension17': dataDimension17,
-        'dataDimension18': dataDimension18,
-        'img': img["src"]}
-
-    updated = list(set(newBags) - set(dbBags))
-    print(updated)
-    db.bags.insert_one(doc)
+# ?! 기존 db에서 가져온 리스트 (dbBags) - 새로 크롤링한 리스트 (newBags) 비교하기
+    #updated = list(set(newBags) - set(dbBags))
+    #print(updated)
+    #db.bags.insert_one(doc)
 driver.quit()
-# if len(C) != 0 :
-# print("메일쏴주기!!!")
+
+# ?! 차이나는 값의 productId, datadimension17, datadimension18, img 이메일로 보내주기
+
