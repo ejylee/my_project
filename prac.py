a04e120053d2bad2e8348efb7edc00d711d85f8d
jungyoo <dallim0524@gmail.com>
2021-01-09 14:32:21 +0900

    Update prac.py
    크롤링한 값들 비교해서 다른 값 출력함

--- prac.py
+++ prac.py
@@ -6,8 +6,13 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-# ?! 기존 db에 저장되어있던 최신값 가져오기 어떻게하지? -> db.foo.find().sort({x:-1})
-# ?! 가져온 값들을 리스트형태로 어떻게 만들지? -> dbBags 리스트 만들기
+
+# 기존 db에 저장되어있던 최신값 가져오기
+db_data = list(db.bags.find().limit(1))
+# 가져온 값들 중 productID 리스트로 만들기
+list1 = db_data[0]['productId']
+print("여기는 list1")
+print(list1)
 
 # 새로 크롤링하기 (newBags 리스트 만들기)
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -30,13 +35,24 @@ for bag in bags:
     dataDimension17.append(bag['data-dimension17'])
     dataDimension18.append(bag['data-dimension18'])
     img.append(bag.select_one('img'))
+
+list2 = productId
+list2.append("가짜수")
+
 newBags = {'productId': productId}
+print("여기는newBags")
 print(newBags)
 
 # ?! 기존 db에서 가져온 리스트 (dbBags) - 새로 크롤링한 리스트 (newBags) 비교하기
-    #updated = list(set(newBags) - set(dbBags))
-    #print(updated)
-    #db.bags.insert_one(doc)
+updated = list(set(list2) - set(list1))
+print(list2)
+print("결과값!!!!@#!@#!@#!@$!@$!@")
+print(updated)
+if len(updated) == 0 :
+    print("no change")
+else :
+    print("변한게 있어서 메일 보내줘야함!!!!")
+#db.bags.insert_one(newBags)
 driver.quit()
 
 # ?! 차이나는 값의 productId, datadimension17, datadimension18, img 이메일로 보내주기
